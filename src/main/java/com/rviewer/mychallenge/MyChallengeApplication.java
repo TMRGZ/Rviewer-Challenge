7baf46d022c9c67afd75e1181e6e3d671b071b55
Miguel Ángel <MiguelRG5605@Outlook.es>
2023-08-26 16:37:31 +0200

    First commit

---
+++ src/main/java/com/rviewer/mychallenge/MyChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.rviewer.mychallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyChallengeApplication.class, args);
+	}
+
+}
