ec1279ac880f92af92589a8bd0a9f9f2dd51b906
Miguel Ángel <MiguelRG5605@Outlook.es>
2023-08-26 19:05:44 +0200

    FEATURE: Added generic abstract application mapper

---
+++ src/main/java/com/rviewer/mychallenge/application/mapper/common/GenericDtoMapper.java
@@ -0,0 +1,14 @@
+package com.rviewer.mychallenge.application.mapper.common;
+
+import com.rviewer.mychallenge.application.dto.common.CmdbElementDto;
+import com.rviewer.mychallenge.domain.model.common.CmdbElement;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface GenericDtoMapper<M extends CmdbElement<I>, D extends CmdbElementDto<I>, I> {
+
+    M mapToModel(D dto);
+
+    D mapToDto(M model);
+
+}
