26435bd0e77fcc9269d7f9d00bdf92264ea3be78
Miguel Ángel <MiguelRG5605@Outlook.es>
2023-08-31 18:52:25 +0200

    FEATURE: Fixed ambiguous call

--- src/main/java/com/rviewer/mychallenge/infrastructure/persistence/repository/common/jpa/JpaElementCrudRepository.java
+++ src/main/java/com/rviewer/mychallenge/infrastructure/persistence/repository/common/jpa/JpaElementCrudRepository.java
@@ -3,10 +3,16 @@ package com.rviewer.mychallenge.infrastructure.persistence.repository.common.jpa
 import com.rviewer.mychallenge.infrastructure.persistence.dao.common.CmdbElementDao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.lang.NonNull;
 
 import java.io.Serializable;
+import java.util.Optional;
 
 @NoRepositoryBean
-public interface JpaElementCrudRepository<E extends CmdbElementDao<I>, I extends Serializable>
-        extends JpaElementReadOnlyRepository<E, I>, JpaRepository<E, I> {
+public interface JpaElementCrudRepository<D extends CmdbElementDao<I>, I extends Serializable>
+        extends JpaElementReadOnlyRepository<D, I>, JpaRepository<D, I> {
+
+    @NonNull
+    @Override
+    Optional<D> findById(@NonNull I id);
 }
