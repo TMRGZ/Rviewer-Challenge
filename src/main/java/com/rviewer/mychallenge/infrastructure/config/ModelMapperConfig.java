c78beaef688d327347ef3dce8e3f8be1e5bad7cf
Miguel Ángel <MiguelRG5605@Outlook.es>
2023-11-11 20:17:08 +0100

    FEATURE: Added modelmapper

---
+++ src/main/java/com/rviewer/mychallenge/infrastructure/config/ModelMapperConfig.java
@@ -0,0 +1,18 @@
+package com.rviewer.mychallenge.infrastructure.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.record.RecordModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.registerModule(new RecordModule());
+        return modelMapper;
+    }
+
+}
